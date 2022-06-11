48181c7199ffcbc4df3b61d098f9a90c0a98a509
rcastillod <rodrigo.castillo@global3.cl>
2022-06-10 22:51:06 -0400

    Components en vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main-wrapper">
+      <div class="personajes">
+        <personaje 
+          v-for="personaje in personajes" 
+          :key="personaje.id"
+          :id="personaje.id"
+          :name="personaje.name"
+          :status="personaje.status"
+          :species="personaje.species"
+          :imageSrc="personaje.imageSrc"
+        ></personaje>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Personaje from '@/components/PersonajeCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'personaje': Personaje
+  },
+  data: function() {
+    return {
+      personajes: [
+        { id: 1, name: "Rick Sanchez", status: "Alive", species: "Human", imageSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg" },
+        { id: 2, name: "Morty Smith", status: "Alive", species: "Human", imageSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg" },
+        { id: 3, name: "Summer Smith", status: "Alive", species: "Human", imageSrc: "https://rickandmortyapi.com/api/character/avatar/3.jpeg" },
+        { id: 4, name: "Beth Smith", status: "Alive", species: "Human", imageSrc: "https://rickandmortyapi.com/api/character/avatar/4.jpeg" }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+:root {
+  --clr-card-bg: 60 69% 62%;
+  --clr-white: 0 0% 100%;
+}
+body {
+  background-image: url('assets/rm-bg.png');
+  background-size: cover;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  display: grid;
+  height: 100vh;
+  place-items: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+}
+.main-wrapper {
+  margin-inline: auto;
+  width: min(100% - 2rem, 68.75rem);
+}
+.personajes {
+  display: grid;
+  column-gap: 1.875rem;
+  row-gap: 6.25rem;
+  grid-template-columns: repeat(auto-fit, minmax(13.4375rem, 1fr));
 }
 </style>
---
+++ src/components/PersonajeCard.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="card">
+        <img :src="imageSrc" :alt="name" class="card__image">
+        <div class="card__content">
+            <div class="card__title">{{name}}</div>
+            <div class="card__meta">
+                <p><span>Estado:</span> {{status}}</p>
+                <p><span>Especie:</span> {{species}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'personaje-comp',
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        },
+        species: {
+            type: String,
+            required: true
+        },
+        imageSrc: {
+            type: String,
+            required: true
+        },
+    },
+    data: function(){
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+    .card {
+        background-color: hsl(var(--clr-card-bg)/.1);
+        backdrop-filter: blur(10px);
+        box-shadow: 
+            inset -1px 0px 0px hsl(var(--clr-white)/.1),
+            inset -1px 2px 4px hsl(var(--clr-white)/.1);
+        border-radius: .625rem;
+        padding: .9375rem 1.875rem 1.875rem;
+    }
+    .card__image {
+        border-radius: 50%;
+        border: 3px solid hsl(var(--clr-card-bg));
+        margin-top: -6.25rem;
+        margin-left: -.9375rem;
+        height: 6.25rem;
+        width: 6.25rem;
+    }
+    .card__content {
+        color: hsl(var(--clr-white));
+        margin-top: 1.25rem;
+    }
+    .card__title {
+        font-size: 1.5rem;
+        font-weight: bold;
+        margin-bottom: .3125rem;
+    }
+    .card__meta {
+        display: flex;
+        flex-direction: column;
+    }
+    .card__meta p {
+        margin: 0;
+    }
+    .card__meta span {
+        font-weight: bold;
+    }
+</style>
